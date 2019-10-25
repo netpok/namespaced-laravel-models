ec2a6bc7ae18371739a8dd126a61f337576b6022
Balázs Váradi <netpok@gmail.com>
2019-10-25 22:30:57 +0200

    Add namespace fixer

---
+++ src/ModelNamespaceFixer.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Netpok\NamespacedLaravelModels;
+
+use InvalidArgumentException;
+use Symfony\Component\Console\Input\InputInterface;
+
+class ModelNamespaceFixer
+{
+    protected $rootNamespace;
+    protected $modelNamespace;
+
+    public function __construct($rootNamespace, $modelNamespace){
+        $this->rootNamespace = $rootNamespace;
+        $this->modelNamespace = $modelNamespace;
+    }
+
+    public function fixInput(string $command, InputInterface $input): void
+    {
+        switch($command){
+            case 'make:model':
+                $this->handleArgument($input);
+                return;
+            case 'make:controller':
+                $this->handleOption($input, 'parent');
+                // no break
+            case 'make:factory':
+            case 'make:observer':
+            case 'make:policy':
+                $this->handleOption($input, 'model');
+                return;
+            default:
+                throw new InvalidArgumentException(
+                    sprintf('Command [%s] not supported', $command)
+                );
+        }
+    }
+
+    protected function handleArgument(InputInterface $input): void
+    {
+        $input->setArgument(
+            'name',
+            dump($this->prefixClass($input->getArgument('name')))
+        );
+    }
+
+    protected function handleOption(InputInterface $input, string $name): void
+    {
+        if ($option = $input->getOption($name)) {
+            $input->setOption($name, $this->prefixClass($option));
+        }
+    }
+
+    /**
+     * Parse the class name and format according to the root namespace.
+     *
+     * @param  string  $name
+     * @return string
+     */
+    protected function prefixClass(string $name): string
+    {
+        $name = ltrim($name, '\\/');
+
+        if (\Str::startsWith($name, $this->rootNamespace)) {
+            return $name;
+        }
+
+        $name = $this->modelNamespace.'\\'.str_replace('/', '\\', $name);
+
+        return trim($this->rootNamespace, '\\').'\\'.$name;
+    }
+}
